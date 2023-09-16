26a646bf456745e53e6162e6b20030a787bba0d8
YevhenBez <byxgalter@bigmir.net>
2023-09-16 18:48:11 +0300

    Написал   carsSlice   и написал для него   builder   Тестовое задание (аналог тестового с реального собеседования).
    Написал   carsSlice   и написал для него   builder   Для этого я импортнул   createSlice   и   fetchCars   Также в переменной   carsSlice   я уже использовал   extraReducers   т.к. функции для Билдера я беру за пределами данной переменной   Далее использую метод Билдера    addCase     когда колбэки при Промисе     fulfilled    разные (    handleFulfilled    - получение всех автомобилей А метод Билдера    addMatcher    использую тогда, когда по значению Промиса    /pending     ( к данному значению привязывается команда     action => action.type.endsWith     ) всегда одинаковый колбэк    handlePending    Аналогичная логика с значением Промиса    /rejected     только там свой колбэк    handleRejected   Также в переменной   carsReducer    получил Редьюсер.    Тестовое задание (аналог тестового с реального собеседования).

--- src/redux/carsSlice.js
+++ src/redux/carsSlice.js
@@ -1,3 +1,6 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchCars } from '../redux/operations';
+
 const handlePending = state => {
   state.isLoading = true;
 };
@@ -18,3 +21,16 @@ const carsInitialState = {
   isLoading: false,
   error: null,
 };
+
+export const carsSlice = createSlice({
+  name: 'cars',
+  initialState: carsInitialState,
+  extraReducers: builder => {
+    builder
+      .addCase(fetchCars.fulfilled, handleFulfilled)
+      .addMatcher(action => action.type.endsWith('/pending'), handlePending)
+      .addMatcher(action => action.type.endsWith('/rejected'), handleRejected);
+  },
+});
+
+export const carsReducer = carsSlice.reducer;
