a25a45ba2d54b7ec0ea2deb797040d6134111b1b
YevhenBez <115114239+YevhenBez@users.noreply.github.com>
2023-09-16 13:04:52 +0000

    Deploying to gh-pages from @ YevhenBez/test-tech-part-2@ebddab2e02aec2213be1cd97547b29c959a24f61 🚀

---
+++ static/js/927.036aeb1c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[927],{927:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:(0,a.jsx)("h2",{children:"Favorites"})})}}}]);
+//# sourceMappingURL=927.036aeb1c.chunk.js.map
