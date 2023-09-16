ca3ce625e38b2272f0d6f4e0516211bf006b9f70
YevhenBez <byxgalter@bigmir.net>
2023-09-16 18:20:41 +0300

    Прописал колбэки для    builder     которые буду использовать в    carsSlice     Также задал начальное значение для массива автомобилей.  Тестовое задание (аналог тестового с реального собеседования).
    Прописал колбэки для    builder     которые буду использовать в    carsSlice     Данные колбэки будут менять начальные Стейты     isLoading     error     и   arrayInitialCars  Также в переменной  carsInitialState  задал начальное значение для массива автомобилей   arrayInitialCars   .  Тестовое задание (аналог тестового с реального собеседования).

--- src/redux/carsSlice.js
+++ src/redux/carsSlice.js
@@ -1,5 +1,20 @@
+const handlePending = state => {
+  state.isLoading = true;
+};
+
+const handleFulfilled = (state, { payload }) => {
+  state.isLoading = false;
+  state.error = null;
+  state.arrayInitialCars = payload;
+};
+
+const handleRejected = (state, { payload }) => {
+  state.isLoading = false;
+  state.error = payload;
+};
+
 const carsInitialState = {
-  
+  arrayInitialCars: [],
   isLoading: false,
   error: null,
 };
