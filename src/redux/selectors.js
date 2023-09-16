850c35708d1eb19c4364d7b7ece3688e6cbc7604
YevhenBez <byxgalter@bigmir.net>
2023-09-16 19:09:26 +0300

    Написал селекторы.  Тестовое задание (аналог тестового с реального собеседования).
    Написал селекторы. А именно  selectCars для получения массивов автомобилей.  selectIsLoading   признак, что массив автомобилей ещё загружается.  selectError   ошибка  Тестовое задание (аналог тестового с реального собеседования).

---
+++ src/redux/selectors.js
@@ -0,0 +1,4 @@
+export const selectCars = state => state.cars.arrayInitialCars;
+
+export const selectIsLoading = state => state.cars.isLoading;
+export const selectError = state => state.cars.error;
