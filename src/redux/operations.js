1eab20e4ad01431a96c6192b14083e663885a8bc
YevhenBez <byxgalter@bigmir.net>
2023-09-16 18:01:37 +0300

    Написал createAsyncThunk для получения авто с Бэкенда. Тестовое задание (аналог тестового с реального собеседования).
    Написал createAsyncThunk для получения авто с Бэкенда. Для этого импортнул    createAsyncThunk    , а в переменной  fetchCars   прописал логику данного     createAsyncThunk    Первым аргументом он принимает тип экшена (в нашем случае  cars/fetchAll   ) -   это значит, что работаем с автомобилями и хотим получить все автомобили. На самом деле значение     fetchAll    не зарезервировано. Можно написать всё что захочешь),   а вторым аргументом он принимает    функцию, которая должна выполнить HTTP-запрос и вернуть промис с данными, которые станут значениями    payload.   В данной функции, которая делает  HTTP-запрос, первое значение пропускаем, т.к. мы получаем все автомобили, а второе значение данной функции зарезервировано - это    thunkAPI    , деструктуризировав который мы можем получить его метод    rejectWithValue    (перевод которого говорит сам за себя). Обрати внимание, что в переменной   response   метод   get   обращается именно к   adverts    Т.к. так называется ресурс с данными на   https://mockapi.io   , который в данном проекте ты используешь как Бэкэнд.   Тестовое задание (аналог тестового с реального собеседования).

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,3 +1,16 @@
 import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = 'https://649054391e6aa71680cb0a3b.mockapi.io/api/v1/';
+
+export const fetchCars = createAsyncThunk(
+  'cars/fetchAll',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await axios.get('/adverts');
+        return response.data;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
